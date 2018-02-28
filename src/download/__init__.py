0d0b8857b16f6ececf3f5a779bc74323b8d44031
yuxuehai@meizu.com <yuxuehai@meizu.com>
2018-02-28 20:31:10 +0800

    draw temperature feature

--- src/download/__init__.py
+++ src/download/__init__.py
@@ -3,7 +3,10 @@
 
 import csv
 
-filename = 'sitka_weather_07-2014.csv'
+from matplotlib import pyplot as plt
+from datetime import datetime
+
+filename = 'sitka_weather_2014.csv'
 
 with open(filename) as f:
    reader = csv.reader(f)
@@ -13,8 +16,34 @@ with open(filename) as f:
    for index , column_header in enumerate(header_row):
        print(index, column_header)
 
-   highs = []
+   dates, highs, lows= [], [], []
    for row in reader:
-       high = int(row[1])
-       highs.append(high)
+       try:
+           current_date = datetime.strptime(row[0], "%Y-%m-%d")
+           high = int(row[1])
+           low = int(row[3])
+       except ValueError:
+           print(current_date, 'missing date')
+       else:
+           dates.append(current_date)
+           highs.append(high)
+           lows.append(low)
    print(highs)
+
+# 根据数据绘制图像
+
+fig = plt.figure(dpi=128, figsize=(10,6))
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.5)
+
+
+# 设置图像格式
+
+plt.title("Daily low and high temperatures, 2014", fontsize = 24)
+plt.xlabel('', fontsize = 16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature(F)", fontsize = 16)
+plt.tick_params(axis='both', which='major', labelsize = 16)
+
+plt.show()
