82c1978dcd63a75510d9583f74da1b68b6b542b5
yuxuehai@meizu.com <yuxuehai@meizu.com>
2018-03-07 19:37:57 +0800

    Api

--- src/Web/__init__.py
+++ src/Web/__init__.py
@@ -25,5 +25,14 @@ print("\n Keys: ", len(repo_dict))
 for key in sorted(repo_dict.keys()):
     print(key)
 
+print("\nSelected information about first repository:")
+print('Name:', repo_dict['name'])
+print('Owner:', repo_dict['owner']['login'])
+print('Stars:', repo_dict['stargazers_count'])
+print('Repository:', repo_dict['html_url'])
+print('Created:', repo_dict['created_at'])
+print('Updated:', repo_dict['updated_at'])
+print('Description:', repo_dict['description'])
+
 #处理结果
 print(response_dict.keys())
